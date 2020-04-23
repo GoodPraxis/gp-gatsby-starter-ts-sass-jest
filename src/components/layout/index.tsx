8217af10380abb221a5f953186b54bb983d2842a
Maciek Baron <maciej@goodpraxis.uk>
2020-04-23 12:20:40 +0100

    Remove comment

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 
 const Layout: React.FC = ({ children } : { children: React.ReactNode }) => (
