b2a466a0ce4c675799638cb15c54893a18914679
Maciek Baron <maciej@goodpraxis.uk>
2020-04-23 12:20:10 +0100

    Simpliy definition by removing prop validation

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,18 +6,13 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Layout: React.FC = ({ children }) => (
+const Layout: React.FC = ({ children } : { children: React.ReactNode }) => (
   <>
     <div>
       <main>{children}</main>
     </div>
   </>
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Layout;
