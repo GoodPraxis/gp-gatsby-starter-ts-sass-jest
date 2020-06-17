681588e3d8d6e7278caafd3bebd98dc81c240672
Maciek Baron <maciej@goodpraxis.uk>
2020-06-17 17:11:20 +0100

    Add Enzyme

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,5 @@ module.exports = {
     __PATH_PREFIX__: '',
   },
   testURL: 'http://localhost',
-  setupFiles: ['<rootDir>/loadershim.js'],
+  setupFiles: ['<rootDir>/loadershim.js', '<rootDir>/src/setupTests.js'],
 };
