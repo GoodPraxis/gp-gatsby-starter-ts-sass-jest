73587d1291ff7b0bc63e72df9814560daa9ef4f0
Maciek Baron <maciej@goodpraxis.uk>
2021-04-09 16:59:17 +0100

    Update major versions of packages

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 import { configure } from 'enzyme';
 // eslint-disable-next-line
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'; // @TODO: Change after update
 
 configure({ adapter: new Adapter() });
